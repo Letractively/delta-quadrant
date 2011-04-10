f29b8475e003b2cb73672f5f31c44ba5709fe261
k42b3.x <k42b3.x@10fdba5f-8f47-7826-6372-eaa9f6058312>
2011-04-10 10:59:33 +0000

    update sh deamon

--- xoxa/xoxad.sh
+++ xoxa/xoxad.sh
@@ -1,25 +1,20 @@
-#
-# Xoxa startup script.
-#
-
-name="XoXa"
-
-case "$1" in
-	start)
-		echo ""
-		echo "Starting xoxa ..."
-		echo ""
-		java -jar /home/k42b3/xoxa_0.0.2_beta.jar > /dev/null
-		;;
-	stop)
-		echo ""
-		echo "Stopping xoxa ..."
-		echo ""
-		pkill -f java > /dev/null 2>&1
-		;;
-	*)
-		echo ""
-		echo "Usage: ${name} { start | stop }"
-		echo ""
-		exit 64
-		;;
+#!/bin/sh
+case $1 in
+  stop)
+  echo
+  echo "Stopping xoxa ..."
+  echo
+  pkill -f java > /dev/null 2>&1
+  ;;
+  start)
+  echo
+  echo "Starting xoxa ..."
+  echo
+  nohup java -jar /home/k42b3/xoxa_0.0.2_beta.jar > /dev/null
+  ;;
+  *)
+  echo
+  echo "Usage: xoxad.sh { start | stop }"
+  echo
+  ;;
+esac
