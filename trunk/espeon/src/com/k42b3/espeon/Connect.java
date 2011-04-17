0ec9b4713fb9b9d72389dc0acf112a9b10770215
k42b3.x <k42b3.x@10fdba5f-8f47-7826-6372-eaa9f6058312>
2011-04-17 19:35:34 +0000

    update codestyle

---
+++ trunk/espeon/src/com/k42b3/espeon/Connect.java
@@ -0,0 +1,193 @@
+/**
+ * espeon
+ * 
+ * With espeon you can generate sourcecode from database structures. It was 
+ * mainly developed to generate PHP classes for the psx framework (phpsx.org) 
+ * but because it uses a template engine (FreeMarker) you can use it for any 
+ * purpose you like.
+ * 
+ * Copyright (c) 2010 Christoph Kappestein <k42b3.x@gmail.com>
+ * 
+ * This file is part of espeon. espeon is free software: you can 
+ * redistribute it and/or modify it under the terms of the GNU 
+ * General Public License as published by the Free Software Foundation, 
+ * either version 3 of the License, or at any later version.
+ * 
+ * espeon is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with espeon. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.k42b3.espeon;
+
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+/**
+ * connect
+ *
+ * @author     Christoph Kappestein <k42b3.x@gmail.com>
+ * @license    http://www.gnu.org/licenses/gpl.html GPLv3
+ * @link       http://code.google.com/p/delta-quadrant
+ * @version    $Revision: 33 $
+ */
+public class Connect extends JFrame
+{
+	public static boolean isActive = false;
+	
+	private JTextField txtHost;
+	private JTextField txtDb;
+	private JTextField txtUser;
+	private JTextField txtPw;
+
+	private JButton btnConnect;
+	private JButton btnCancel;
+	
+	private ConnectInterface callback;
+	
+	public Connect()
+	{
+		Connect.isActive = true;
+
+		this.setLocationRelativeTo(null);
+		
+		this.setSize(200, 180);
+
+		this.setMinimumSize(this.getSize());
+
+		this.setResizable(false);
+
+		this.setTitle("Connect");
+
+		this.setLayout(new GridLayout(0, 1));
+
+
+		JPanel panelHost = new JPanel();
+
+		panelHost.setLayout(new FlowLayout());
+
+		JLabel lblHost = new JLabel("Host:");
+		lblHost.setPreferredSize(new Dimension(80, 20));
+		
+		this.txtHost = new JTextField();
+		this.txtHost.setText("localhost");
+		this.txtHost.setPreferredSize(new Dimension(120, 24));
+		
+		panelHost.add(lblHost);
+		panelHost.add(this.txtHost);
+		
+		this.add(panelHost);
+		
+		
+		JPanel panelDb = new JPanel();
+		
+		panelDb.setLayout(new FlowLayout());
+
+		JLabel lblDb = new JLabel("Database:");
+		lblDb.setPreferredSize(new Dimension(80, 20));
+		
+		this.txtDb = new JTextField();
+		this.txtDb.setText("cms");
+		this.txtDb.setPreferredSize(new Dimension(120, 24));
+		
+		panelDb.add(lblDb);
+		panelDb.add(this.txtDb);
+		
+		this.add(panelDb);
+		
+		
+		JPanel panelUser = new JPanel();
+		
+		panelUser.setLayout(new FlowLayout());
+
+		JLabel lblUser = new JLabel("User:");
+		lblUser.setPreferredSize(new Dimension(80, 20));
+		
+		this.txtUser = new JTextField();
+		this.txtUser.setText("root");
+		this.txtUser.setPreferredSize(new Dimension(120, 24));
+		
+		panelUser.add(lblUser);
+		panelUser.add(this.txtUser);
+		
+		this.add(panelUser);
+		
+
+		JPanel panelPw = new JPanel();
+		
+		panelPw.setLayout(new FlowLayout());
+
+		JLabel lblPw = new JLabel("Password:");
+		lblPw.setPreferredSize(new Dimension(80, 20));
+		
+		this.txtPw = new JTextField();
+		this.txtPw.setPreferredSize(new Dimension(120, 24));
+		
+		panelPw.add(lblPw);
+		panelPw.add(this.txtPw);
+		
+		this.add(panelPw);
+		
+		
+		JPanel panelButtons = new JPanel();
+		
+		panelButtons.setLayout(new FlowLayout());
+		
+		this.btnConnect = new JButton("Connect");
+		this.btnConnect.setPreferredSize(new Dimension(100, 24));
+		this.btnConnect.addActionListener(new connectHandler());
+		
+		panelButtons.add(this.btnConnect);
+		
+		this.btnCancel = new JButton("Cancel");
+		this.btnCancel.setPreferredSize(new Dimension(100, 24));
+		this.btnCancel.addActionListener(new cancelHandler());
+		
+		panelButtons.add(this.btnCancel);
+		
+		this.add(panelButtons);
+	}
+	
+	public void setCallback(ConnectInterface callback)
+	{
+		this.callback = callback;
+	}
+
+	public void close()
+	{
+		setVisible(false);
+		
+		Connect.isActive = false;
+	}
+	
+	public class connectHandler implements ActionListener
+	{
+		public void actionPerformed(ActionEvent e) 
+		{
+			callback.connect(txtHost.getText(), txtDb.getText(), txtUser.getText(), txtPw.getText());
+
+			close();
+		}
+	}
+	
+	public class cancelHandler implements ActionListener
+	{
+		public void actionPerformed(ActionEvent e) 
+		{
+			close();
+		}
+	}
+}
---
+++ trunk/espeon/src/com/k42b3/espeon/Espeon.java
@@ -0,0 +1,431 @@
+/**
+ * espeon
+ * 
+ * With espeon you can generate sourcecode from database structures. It was 
+ * mainly developed to generate PHP classes for the psx framework (phpsx.org) 
+ * but because it uses a template engine (FreeMarker) you can use it for any 
+ * purpose you like.
+ * 
+ * Copyright (c) 2010 Christoph Kappestein <k42b3.x@gmail.com>
+ * 
+ * This file is part of espeon. espeon is free software: you can 
+ * redistribute it and/or modify it under the terms of the GNU 
+ * General Public License as published by the Free Software Foundation, 
+ * either version 3 of the License, or at any later version.
+ * 
+ * espeon is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with espeon. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.k42b3.espeon;
+
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.InputStream;
+import java.io.OutputStreamWriter;
+import java.io.Writer;
+import java.net.URI;
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import javax.swing.BorderFactory;
+import javax.swing.DefaultListModel;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JList;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.ScrollPaneConstants;
+import javax.swing.SwingUtilities;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+
+import com.k42b3.espeon.model.Table;
+
+import freemarker.template.Configuration;
+import freemarker.template.DefaultObjectWrapper;
+import freemarker.template.Template;
+
+/**
+ * espeon
+ *
+ * @author     Christoph Kappestein <k42b3.x@gmail.com>
+ * @license    http://www.gnu.org/licenses/gpl.html GPLv3
+ * @link       http://code.google.com/p/delta-quadrant
+ * @version    $Revision: 36 $
+ */
+public class Espeon extends JFrame
+{
+	public static String ver = "0.0.2 beta";
+	public static String path = "templates";
+
+	private JList list;
+	private DefaultListModel lm;
+	private JTable table;
+	private Table tm;
+	private Toolbar toolbar;
+	
+	private Connection con;
+	
+	private Configuration cfg;
+	
+	public Espeon()
+	{
+		this.setTitle("espeon (version: " + ver + ")");
+		
+		this.setLocation(100, 100);
+		
+		this.setSize(600, 500);
+		
+		this.setMinimumSize(this.getSize());
+
+		this.setLayout(new BorderLayout());
+
+
+		// columns
+		JPanel panelColumns = new JPanel();
+
+		this.table = new JTable();
+
+		this.tm = new Table();
+
+		this.table.setModel(this.tm);
+
+		this.table.setEnabled(false);
+		
+		JScrollPane scrTable = new JScrollPane(this.table);
+		
+		scrTable.setBorder(BorderFactory.createEmptyBorder(4, 0, 4, 4));		
+		
+		this.add(scrTable, BorderLayout.CENTER);
+
+
+		// toolbar
+		this.toolbar = new Toolbar();
+		
+		this.toolbar.getConnect().addActionListener(new connectHandler());
+		
+		this.toolbar.getGenerate().addActionListener(new generateHandler());
+		
+		this.toolbar.getGenerate().setEnabled(false);
+		
+		this.toolbar.getAbout().addActionListener(new aboutHandler());
+		
+		this.toolbar.getExit().addActionListener(new exitHandler());
+
+		this.add(this.toolbar, BorderLayout.SOUTH);
+
+
+		// list
+		this.lm = new DefaultListModel(); 
+		
+		this.list = new JList(this.lm);
+
+		this.list.setEnabled(false);
+		
+		//this.list.setModel(new file_list());
+
+		this.list.addListSelectionListener(new listHandler());
+
+		JScrollPane scrList = new JScrollPane(this.list);
+		
+		scrList.setBorder(BorderFactory.createEmptyBorder(4, 4, 4, 4));
+		
+		scrList.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+
+		scrList.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+
+		scrList.setPreferredSize(new Dimension(128, 400));
+
+		this.add(scrList, BorderLayout.WEST);
+
+
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+		// set template config
+		try
+		{
+			File templatePath = new File(Espeon.path);
+			
+			if(templatePath.isDirectory())
+			{
+				this.cfg = new Configuration();
+
+				this.cfg.setDirectoryForTemplateLoading(new File(Espeon.path));
+
+				this.cfg.setObjectWrapper(new DefaultObjectWrapper());
+			}
+			else
+			{
+				throw new Exception("You have to create a dir called '" + Espeon.path + "' where the templates are located");
+			}
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	public class connectHandler implements ActionListener
+	{
+		public void actionPerformed(ActionEvent e) 
+		{
+			SwingUtilities.invokeLater(new Runnable(){
+
+				public void run() 
+				{
+					Connect win = new Connect();
+
+					win.pack();
+
+					win.setCallback(new ConnectInterface(){
+
+						public void connect(String host, String db, String user, String pw) {
+
+							try
+							{
+								// connect
+								con = DriverManager.getConnection("jdbc:mysql://" + host + "/" + db + "?user=" + user + "&amp;password=" + pw);
+
+
+								// list tables
+								PreparedStatement ps = con.prepareStatement("SHOW TABLES");
+
+								ps.execute();
+
+								ResultSet result = ps.getResultSet();
+
+								while(result.next())
+								{
+									lm.addElement(result.getString(1));
+								}
+
+
+								// enable/disable buttons
+								toolbar.getGenerate().setEnabled(true);
+								toolbar.getConnect().setEnabled(false);
+								
+								list.setEnabled(true);
+								table.setEnabled(true);
+							}
+							catch(SQLException e)
+							{
+								JOptionPane.showMessageDialog(null, e.getMessage(), "SQL Exception", JOptionPane.WARNING_MESSAGE);
+							}
+							catch(Exception e)
+							{
+								e.printStackTrace();
+							}
+
+						}
+
+					});
+
+					win.setVisible(true);
+
+				}
+
+			});
+		}
+	}
+
+	public class generateHandler implements ActionListener
+	{
+		private HashMap<String, Object> table;
+
+		public void actionPerformed(ActionEvent e) 
+		{
+			if(list.getSelectedIndex() != -1)
+			{
+				this.table = new HashMap<String, Object>();
+
+				Object firstColumn = "";
+				Object lastColumn = "";
+				Object primaryKey = "";
+				ArrayList<Object> unqiueKey = new ArrayList<Object>();
+				ArrayList<Object> fields = new ArrayList<Object>();
+				ArrayList<HashMap<String, String>> columns = new ArrayList<HashMap<String, String>>();
+
+				for(int i = 0; i < tm.getRowCount(); i++)
+				{
+					if((Boolean) tm.getValueAt(i, 0))
+					{
+						String rField = tm.getValueAt(i, 1) != null ? tm.getValueAt(i, 1).toString() : "";
+						String rType = tm.getValueAt(i, 2) != null ? tm.getValueAt(i, 2).toString() : "";
+						String rNull = tm.getValueAt(i, 3) != null ? tm.getValueAt(i, 3).toString() : "";
+						String rKey = tm.getValueAt(i, 4) != null ? tm.getValueAt(i, 4).toString() : "";
+						String rDefault = tm.getValueAt(i, 5) != null ? tm.getValueAt(i, 5).toString() : "";
+						String rExtra = tm.getValueAt(i, 6) != null ? tm.getValueAt(i, 6).toString() : "";
+
+
+						lastColumn = rField;
+
+						if(i == 0)
+						{
+							firstColumn = rField;
+						}
+
+						if(rKey.equals("PRI"))
+						{
+							primaryKey = rField;
+						}
+
+						if(rKey.equals("UNI"))
+						{
+							unqiueKey.add(rField);
+						}
+
+
+						fields.add(rField);
+
+
+						String rLength = "";
+						int pos = rType.indexOf('(');
+
+						if(pos != -1)
+						{
+							String rawLength = rType.substring(pos + 1);
+							rLength = rawLength.substring(0, rawLength.length() - 1);
+							rType = rType.substring(0, pos);
+						}
+
+						HashMap<String, String> c = new HashMap<String, String>();
+
+						c.put("field", rField);
+						c.put("type", rType);
+						c.put("length", rLength);
+						c.put("null", rNull);
+						c.put("key", rKey);
+						c.put("default", rDefault);
+						c.put("extra", rExtra);
+
+						columns.add(c);
+					}
+				}
+
+				this.table.put("table", list.getSelectedValue());
+				this.table.put("firstColumn", firstColumn);
+				this.table.put("lastColumn", lastColumn);
+				this.table.put("primaryKey", primaryKey);
+				this.table.put("unqiueKey", unqiueKey);
+				this.table.put("fields", fields);
+				this.table.put("columns", columns);
+
+
+				SwingUtilities.invokeLater(new Runnable(){
+
+					public void run() 
+					{
+						Generate win = new Generate(table);
+
+						win.pack();
+						
+						win.setCallback(new GenerateInterface(){
+
+							public void generate(ArrayList<String> templates, HashMap<String, Object> table)
+							{
+								try
+								{
+									/*
+									JFileChooser jfc = new JFileChooser();
+									jfc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+
+									if(jfc.showSaveDialog(espeon.this) == JFileChooser.APPROVE_OPTION)
+									{
+									}
+									*/
+
+									for(int i = 0; i < templates.size(); i++)
+									{
+										Template temp = cfg.getTemplate(templates.get(i));
+
+										Writer out = new FileWriter(templates.get(i));
+
+										temp.process(table, out);
+
+										out.flush();
+									}
+
+									JOptionPane.showMessageDialog(null, "You have successful generated the code", "Informations", JOptionPane.INFORMATION_MESSAGE);
+								}
+								catch(Exception ex)
+								{
+									ex.printStackTrace();
+								}
+							}
+
+						});
+
+						win.setVisible(true);
+					}
+					
+				});
+			}
+			else
+			{
+				JOptionPane.showMessageDialog(null, "Please select a table", "Information", JOptionPane.INFORMATION_MESSAGE);
+			}
+		}
+	}
+	
+	public class aboutHandler implements ActionListener
+	{
+		public void actionPerformed(ActionEvent e) 
+		{
+			StringBuilder out = new StringBuilder();
+			
+			out.append("espeon version " + ver + "\n");
+			out.append("author: Christoph \"k42b3\" Kappestein" + "\n");
+			out.append("website: http://code.google.com/p/delta-quadrant" + "\n");
+			out.append("license: GPLv3 <http://www.gnu.org/licenses/gpl-3.0.html>" + "\n");
+			out.append("\n");
+			out.append("This application generates record classes for the psx framework (phpsx.org)" + "\n");
+			out.append("to make the development of new applications easier and with fewer errors." + "\n");
+			out.append("You can connect to a mysql database select a tabel and generate the php classes" + "\n");
+			out.append("\n");
+			out.append("best regards\n");
+			out.append("k42b3\n");
+			
+			JOptionPane.showMessageDialog(null, out, "About", JOptionPane.INFORMATION_MESSAGE);
+		}
+	}
+	
+	public class exitHandler implements ActionListener
+	{
+		public void actionPerformed(ActionEvent e) 
+		{
+			System.exit(0);
+		}
+	}
+	
+	public class listHandler implements ListSelectionListener
+	{
+		public void valueChanged(ListSelectionEvent e) 
+		{
+			if(e.getValueIsAdjusting())
+			{
+				JList list = (JList) e.getSource();
+				
+				tm.loadTable(con, list.getSelectedValue().toString());
+			}
+		}
+	}
+}
