d00057d8caa31da5f95b090dda130b55899cb08a
k42b3.x <k42b3.x@googlemail.com>
2011-06-03 18:15:34 +0000

    update

--- zubat/src/com/k42b3/zubat/CheckboxList.java
+++ zubat/src/com/k42b3/zubat/CheckboxList.java
@@ -3,6 +3,7 @@ package com.k42b3.zubat;
 import java.awt.BorderLayout;
 
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTable;
 
 import com.k42b3.zubat.form.FormElementInterface;
@@ -31,9 +32,9 @@ public class CheckboxList extends JPanel implements FormElementInterface
 			table.setAutoResizeMode(JTable.AUTO_RESIZE_LAST_COLUMN);
 
 			table.getColumnModel().getColumn(0).setMinWidth(60);
-			table.getColumnModel().getColumn(0).setMaxWidth(60);
-
-			this.add(table, BorderLayout.CENTER);
+			table.getColumnModel().getColumn(0).setMaxWidth(60);	
+			
+			this.add(new JScrollPane(table), BorderLayout.CENTER);
 		}
 		catch(Exception e)
 		{
@@ -50,9 +51,9 @@ public class CheckboxList extends JPanel implements FormElementInterface
 
 			if(selected)
 			{
-				values.append(tm.getValueAt(i, 1).toString());
+				values.append(tm.getValueAt(i, 2).toString());
 
-				if(i < tm.getRowCount() - 1)
+				if(i < tm.getRowCount() - 2)
 				{
 					values.append(",");
 				}
