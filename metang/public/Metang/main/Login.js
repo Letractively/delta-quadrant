100865e46e3913d683594cc7ab43aa6a30ed90f1
k42b3.x <k42b3.x@googlemail.com>
2011-09-03 20:10:12 +0000

    add comments and keywords

--- metang/public/Metang/main/Login.js
+++ metang/public/Metang/main/Login.js
@@ -1,4 +1,34 @@
+/*
+ *  $Id$
+ *
+ * metang
+ * An web application to access the API of amun.
+ *
+ * Copyright (c) 2011 Christoph Kappestein <k42b3.x@gmail.com>
+ *
+ * This file is part of metang. metang is free software: you can
+ * redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or any later version.
+ *
+ * metang is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with metang. If not, see <http://www.gnu.org/licenses/>.
+ */
 
+/**
+ * Metang.main.Login
+ *
+ * @author     Christoph Kappestein <k42b3.x@gmail.com>
+ * @license    http://www.gnu.org/licenses/gpl.html GPLv3
+ * @link       http://code.google.com/p/delta-quadrant/
+ * @category   javascript
+ * @version    $Revision$
+ */
 Ext.define('Metang.main.Login', {
 	extend: 'Ext.container.Viewport',
 
