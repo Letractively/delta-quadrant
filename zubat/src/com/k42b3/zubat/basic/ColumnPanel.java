0f3c6cb54946c2a511c9f890d70aff14a9eb98e6
k42b3.x <k42b3.x@gmail.com>
2011-09-21 19:46:12 +0000

    outsource http api connection to neodym library

--- zubat/src/com/k42b3/zubat/basic/ColumnPanel.java
+++ zubat/src/com/k42b3/zubat/basic/ColumnPanel.java
@@ -1,24 +1,25 @@
 /**
- * zubat
+ * $Id$
  * 
+ * zubat
  * An java application to access the API of amun. It is used to debug and
  * control a website based on amun. This is the reference implementation 
  * howto access the api. So feel free to hack and extend.
  * 
  * Copyright (c) 2011 Christoph Kappestein <k42b3.x@gmail.com>
  * 
- * This file is part of oat. oat is free software: you can 
+ * This file is part of zubat. zubat is free software: you can 
  * redistribute it and/or modify it under the terms of the GNU 
  * General Public License as published by the Free Software Foundation, 
  * either version 3 of the License, or at any later version.
  * 
- * oat is distributed in the hope that it will be useful,
+ * zubat is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  * 
  * You should have received a copy of the GNU General Public License
- * along with oat. If not, see <http://www.gnu.org/licenses/>.
+ * along with zubat. If not, see <http://www.gnu.org/licenses/>.
  */
 
 package com.k42b3.zubat.basic;
@@ -43,8 +44,8 @@ import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
 
+import com.k42b3.neodym.ServiceItem;
 import com.k42b3.zubat.Configuration;
-import com.k42b3.zubat.ServiceItem;
 import com.k42b3.zubat.Zubat;
 
 /**
@@ -53,7 +54,7 @@ import com.k42b3.zubat.Zubat;
  * @author     Christoph Kappestein <k42b3.x@gmail.com>
  * @license    http://www.gnu.org/licenses/gpl.html GPLv3
  * @link       http://code.google.com/p/delta-quadrant
- * @version    $Revision: 139 $
+ * @version    $Revision$
  */
 public class ColumnPanel extends JFrame
 {
--- zubat/src/com/k42b3/zubat/basic/form/CheckboxListTableModel.java
+++ zubat/src/com/k42b3/zubat/basic/form/CheckboxListTableModel.java
@@ -1,24 +1,25 @@
 /**
- * zubat
+ * $Id$
  * 
+ * zubat
  * An java application to access the API of amun. It is used to debug and
  * control a website based on amun. This is the reference implementation 
  * howto access the api. So feel free to hack and extend.
  * 
  * Copyright (c) 2011 Christoph Kappestein <k42b3.x@gmail.com>
  * 
- * This file is part of oat. oat is free software: you can 
+ * This file is part of zubat. zubat is free software: you can 
  * redistribute it and/or modify it under the terms of the GNU 
  * General Public License as published by the Free Software Foundation, 
  * either version 3 of the License, or at any later version.
  * 
- * oat is distributed in the hope that it will be useful,
+ * zubat is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  * 
  * You should have received a copy of the GNU General Public License
- * along with oat. If not, see <http://www.gnu.org/licenses/>.
+ * along with zubat. If not, see <http://www.gnu.org/licenses/>.
  */
 
 package com.k42b3.zubat.basic.form;
@@ -32,7 +33,7 @@ import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
-import com.k42b3.zubat.Http;
+import com.k42b3.neodym.Http;
 
 /**
  * CheckboxListTableModel
@@ -40,7 +41,7 @@ import com.k42b3.zubat.Http;
  * @author     Christoph Kappestein <k42b3.x@gmail.com>
  * @license    http://www.gnu.org/licenses/gpl.html GPLv3
  * @link       http://code.google.com/p/delta-quadrant
- * @version    $Revision: 128 $
+ * @version    $Revision$
  */
 public class CheckboxListTableModel extends AbstractTableModel
 {
