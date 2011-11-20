adb8fa640848134413ac5b5fd3607770e75a55da
k42b3.x@gmail.com <k42b3.x@gmail.com@10fdba5f-8f47-7826-6372-eaa9f6058312>
2011-11-20 23:52:55 +0000

    update doc

--- trunk/kadabra/src/com/k42b3/kadabra/FileMap.java
+++ trunk/kadabra/src/com/k42b3/kadabra/FileMap.java
@@ -1,3 +1,28 @@
+/**
+ * Kadabra
+ * 
+ * Kadabra is an application to mirror a source folder to a destination folder.
+ * You can create multiple projects wich are stored in an SQLite database.
+ * With the option --status [id] you can see wich changes are made and
+ * with --release [id] you update the changes. You can use different handler
+ * like System or FTP.
+ * 
+ * Copyright (c) 2010, 2011 Christoph Kappestein <k42b3.x@gmail.com>
+ * 
+ * This file is part of oat. oat is free software: you can 
+ * redistribute it and/or modify it under the terms of the GNU 
+ * General Public License as published by the Free Software Foundation, 
+ * either version 3 of the License, or at any later version.
+ * 
+ * oat is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with oat. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.k42b3.kadabra;
 
 import java.io.StringReader;
@@ -12,6 +37,14 @@ import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
 import org.xml.sax.InputSource;
 
+/**
+ * FileMap
+ *
+ * @author     Christoph Kappestein <k42b3.x@gmail.com>
+ * @license    http://www.gnu.org/licenses/gpl.html GPLv3
+ * @link       http://code.google.com/p/delta-quadrant
+ * @version    $Revision$
+ */
 public class FileMap 
 {
 	private HandlerAbstract handler;
